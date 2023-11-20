81712b2faacad941d641b284dd2920089692181b
Eduardo Vigna <ejvigna@gmail.com>
2023-11-20 16:23:30 -0300

    primera

---
+++ Final/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Zapatos (models.Model):
+    tipo = models.CharField(max_length=20)
+    color = models.CharField(max_length=20)
+    material = models.CharField(max_length=20)
+    numero = models.ImageField()
+    
+class Cliente (models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=30)
+    condicion = models.CharField(max_length=20)
+
+class proveedor (models.Model):
+    razonsocial = models.CharField(max_length=40)
+    condicion = models.CharField(max_length=20)
+    mail = models.EmailField()
